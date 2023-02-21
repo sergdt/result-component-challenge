29dabf9c826b4384a9ed50c9b775a98ae43bafad
Sergio Dominguez1 <sadoming@bancolombia.com.co>
2023-02-21 11:30:52 -0500

    added initial config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function App() {
+  
+  const theme = createTheme({
+    borderRadius: '1px',
+    palette: {
+      neutral: {
+        main: 'hsl(224, 30%, 27%)',
+        contrastText: '#fff',
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <div className='left-container'>
+        <div className='title-result'>
+          <h1>Your Result</h1>
+        </div>
+        <div className='circle-result'>
+          <h2>76 of 100</h2>
+        </div>
+        <div className='result'>
+          <h1>Great</h1>
+          <p>You scored higher than 65% of the people who have taken these tests.</p>
+        </div>
+      </div>
+      <div className='right-container'>
+        <div className='title-summary'>
+          <h1>Summary</h1>
+        </div>
+        <div className='summary-container'>
+          <ul>
+            <li>Reaction 80/100</li>
+            <li>Memory 92/100</li>
+            <li>Verbal 61/100</li>
+            <li>Visual 72/100</li>
+          </ul>
+          <ThemeProvider theme={theme}>
+            <Button variant="contained" color='neutral'>Continue</Button>
+          </ThemeProvider>
+          
+        </div>
+      </div>
+      
     </div>
   );
 }
